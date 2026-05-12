dddff56e9ae0b00394d44c9d3e526c90863a321a
Hayden <hayden.t.durham@gmail.com>
2026-05-12 11:28:18 -0500

    Created PropertyController

--- src/main/java/rent.properly/property/PropertyController.java
+++ src/main/java/rent.properly/property/PropertyController.java
@@ -1,7 +1,7 @@
 package rent.properly.property;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/properties")
@@ -10,5 +10,26 @@ public class PropertyController {
 
     public PropertyController(PropertyService propertyService) {
         this.propertyService = propertyService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PropertyDto> getPropertyById(@PathVariable Long id) {
+        return ResponseEntity.ok(propertyService.getPropertyById(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<PropertyDto> createProperty(@RequestBody PropertyDto propertyDto) {
+        return ResponseEntity.ok(propertyService.createProperty(propertyDto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PropertyDto> updateProperty(@PathVariable Long id, @RequestBody PropertyDto updatedProperty) {
+        return ResponseEntity.ok(propertyService.updateProperty(id, updatedProperty));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProperty(@PathVariable Long id) {
+        propertyService.deleteProperty(id);
+        return ResponseEntity.ok().build();
     }
 }
